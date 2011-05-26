8e312531f26c865f72b3a261022c73a6df25737d
bizzl <bizzl@users.sourceforge.net>
2011-05-26 13:24:16 +0200

    First functional version.

--- main.php
+++ main.php
@@ -18,7 +18,7 @@ if (!function_exists('join_path')) {
 
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tag.php');
 
-class Nice_Navigation_Widget {
+class Nice_Navigation_Widget extends WP_Widget {
 
 	protected static $domain = 'nice-navigation-widget';
 	protected static $base = '';
@@ -35,7 +35,51 @@ class Nice_Navigation_Widget {
 	public static function init() {
 		self::init_base();
 		self::init_l10n();
+		
+		add_action('widgets_init', array(__CLASS__, 'register_me'));
 	}
+	
+	public static function register_me() {
+		register_widget(__CLASS__);
+	}
+	
+	/* Actual Widget Code */
+	
+	
+
+    function Nice_Navigation_Widget() {
+		/* Widget settings. */
+		$widget_ops = array(
+			'classname' => self::$domain,
+			'description' => __('Adds a sub navigation.', self::$domain)
+		);
+
+		/* Widget control settings. */
+		$control_ops = array(
+			'width' => 300,
+			'height' => 350,
+			'id_base' => self::$domain
+		);
+		
+       parent::WP_Widget(self::$domain, __('Nice Navigation Widget', self::$domain), $widget_ops, $control_ops);
+     }
+
+     function widget($args, $instance) {
+		extract($args);
+		
+		echo $before_widget;
+		echo $before_title;
+		echo __('Nice Navigation Widget', self::$domain);
+		echo $after_title;
+		echo tag('pre')->append('Moin');
+		echo $after_widget;
+     }
+
+     function update($new_instance, $old_instance) {
+     }
+
+     function form($instance) {
+     }
 	
 }
 
