b5a239e9692e3e382c2ccab2b9ed5ab799c123ed
bizzl <bizzl@users.sourceforge.net>
2011-05-26 17:26:04 +0200

    Noww works with menus, too *kotz*

--- main.php
+++ main.php
@@ -73,7 +73,16 @@ class Nice_Navigation_Widget extends WP_Widget {
 	}
 	
 	function get_sub_post_tree($root) {
-		
+		$li = tag('li');
+		$li->append(get_post($root->object_id)->post_title);
+		if (count($root->children)) {
+			$ul = tag('ul');
+			foreach ($root->children as $child)
+				$ul->append($this->get_sub_post_tree($child));
+			$li->append($ul);
+		}
+		return $li;
+			
 	}
 
 	function widget($args, $instance) {
@@ -86,26 +95,59 @@ class Nice_Navigation_Widget extends WP_Widget {
 		if (is_front_page() && !$instance['show_on_home'])
 			return false;
 		
-		$top_most = $post;
-		while ($top_most->post_parent)
-			$top_most = get_post($top_most->post_parent);
+
+		if (count($post->ancestors)) {
+			$top_most = $post;
+			while ($top_most->post_parent)
+				$top_most = get_post($top_most->post_parent);
+			$tree = wp_list_pages(array('child_of' => $top_most->ID, 'title_li' => '', 'echo' => false));
+		} else {
+			$nav = array_shift(get_posts(array('meta_key' => '_menu_item_object_id', 'meta_value' => $post->ID, 'post_type' => 'nav_menu_item')));
+			$menu = array_shift(wp_get_object_terms(array($nav->ID), 'nav_menu'));
+			$menu_items = wp_get_nav_menu_items($menu->term_id);
+			$ided_menu_items = array();
+			foreach ($menu_items as $menu_item) {
+				$menu_item->children = array();
+				$ided_menu_items[$menu_item->ID] = $menu_item;
+			}
+			foreach ($menu_items as $menu_item)
+				if ($menu_item->menu_item_parent) {
+					$ided_menu_items[$menu_item->menu_item_parent]->children[] = $menu_item;
+					$menu_item->root = false;
+				} else
+					$menu_item->root = true;
+			$menu_items = array();
+			foreach($ided_menu_items as $id => $item)
+				if ($item->root)
+					$menu_items[] = $item;
+			$tree = tag('ul');
+			foreach ($menu_items as $menu_item)
+				$tree->append($this->get_sub_post_tree($menu_item));
+		}
 
 		echo $before_widget;
 		echo $before_title;
 		echo apply_filters('widget_title', $instance['title']);
 		echo $after_title;
-		
-		wp_list_pages(array('child_of' => $top_most->ID));
-		
+		echo $tree;
 		echo $after_widget;
 	}
 
 	function update($new_instance, $old_instance) {
-		
+		return array_merge($old_instance, $new_instance);
 	}
 
 	function form($instance) {
-		
+		$instance = wp_parse_args((array)$instance, self::$defaults);
+		echo tag('p')->append(
+			tag('label')->attr('for', $this->get_field_name('title'))->append(__('Title', self::$domain)),
+			tag('input')->attr(array(
+				'type' => 'text',
+				'name' => $this->get_field_name('title'),
+				'id' => $this->get_field_id('title'),
+				'value' => $instance['title'],
+			))
+		);
 	}
 
 }
